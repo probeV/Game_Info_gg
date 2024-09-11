a56bc861a2b9f186c141bb35e361abcf629e58d5
Ahnjaemin1208 <michael1208@naver.com>
2024-09-11 13:54:49 +0900

    CI/CD
    deploy.sh 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app
+PROJECT_NAME=backend
+BUILD_JAR=$(ls /home/ec2-user/app/zip/backend/build/libs/*.jar)
+
+echo "> Build 파일 복사"
+cp $BUILD_JAR $REPOSITORY/
+
+echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료"
+sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
+
+echo "> 새 어플리케이션 배포"
+cd $REPOSITORY
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행 권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
