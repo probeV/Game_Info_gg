9d75983671f33d2c9d110fd57904d147bcb98c29
Ahnjaemin1208 <michael1208@naver.com>
2024-09-11 19:28:05 +0900

    init backend

--- backend/src/main/java/probeV/GamInfogg/GamInfoggApplication.java
+++ backend/src/main/java/probeV/GamInfogg/GamInfoggApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package probeV.GamInfogg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class GamInfoggApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(GamInfoggApplication.class, args);
 	}
 
 }
--- backend/src/test/java/probeV/GamInfogg/GamInfoggApplicationTests.java
+++ backend/src/test/java/probeV/GamInfogg/GamInfoggApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package probeV.GamInfogg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class GamInfoggApplicationTests {
 
 	@Test
 	void contextLoads() {
